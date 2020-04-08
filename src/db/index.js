20560f59a03e3f9f71714fa6a5ef4837380cd2f6
webdev778 <railsruby80@gmail.com>
2020-04-08 22:35:31 +0800

    fix: 🐛 db config

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,12 @@
 var { Pool } = require('pg')
 
-const pool = new Pool()
+const pool = new Pool({
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'polycade',
+  password: process.env.DB_PASS || '',
+  port: process.env.DB_PORT || 5432
+})
 
 pool.query('SELECT NOW()').then(res =>
   console.log(`Database connected ${res.rows[0].now}`))
